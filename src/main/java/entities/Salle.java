27c7093efd36d4d7f253b5b9cabb84714ae98f84
ahmed <ahmedaitkechkech2002@gmail.com>
2025-10-18 19:35:48 +0100

    v1

---
+++ src/main/java/entities/Salle.java
@@ -0,0 +1,58 @@
+package entities;
+
+import java.util.List;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "salles")
+public class Salle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String code;
+
+
+    public Salle(String code) {
+        this.code = code;
+    }
+
+    public Salle() {
+    }
+
+    public Salle(int id, String code, List<Machine> machines) {
+        this.id = id;
+        this.code = code;
+        this.machines = machines;
+    }
+
+    @OneToMany(mappedBy = "salle", fetch = FetchType.EAGER)
+    private List<Machine> machines;
+
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public List<Machine> getMachines() {
+        return machines;
+    }
+
+    public void setMachines(List<Machine> machines) {
+        this.machines = machines;
+    }
+}
